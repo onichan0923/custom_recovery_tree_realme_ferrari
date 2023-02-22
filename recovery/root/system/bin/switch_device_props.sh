b6340c78d81acb6b8fbe949cdb969e0c37036b36
Carlo Dee <carlodee.official@proton.me>
2023-02-23 01:20:10 +0800

    Update.
    * TODO: Fix MTP failing.
    * Use shell command to properly handle ro.product.device depending in ro.separate.soft value.
    
    Signed-off-by: Carlo Dee <carlodee.official@proton.me>

---
+++ recovery/root/system/bin/switch_device_props.sh
@@ -0,0 +1,21 @@
+#!/system/bin/sh
+# This script is needed to load properties for oplus devices
+
+macedonia=$(getprop ro.separate.soft)
+ota_asserts=RMX3300,RMX3301,RE547F,RED8AC,RED8ACL1,ossi,qssi
+
+sleep 3
+
+# Re-set some pecific properties to avoid flashing error
+case $macedonia in
+     "21631")
+          resetprop ro.twrp.target.devices $ota_asserts
+          resetprop ro.product.device RMX3300
+          ;;          
+     "216AC")
+          resetprop ro.twrp.target.devices $ota_asserts
+          resetprop ro.product.device RMX3301
+          ;;
+esac
+
+exit 0
